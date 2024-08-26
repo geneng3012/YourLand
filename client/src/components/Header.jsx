37c07a6f0d67526f79f7bb365258d56997c50b5f
geneng3012 <satyam.anand3012@gmail.com>
2024-08-27 03:33:07 +0530

    Project commit
    Project commi

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,76 @@
+import { FaSearch } from 'react-icons/fa';
+import { Link, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+
+export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
+  return (
+    <header className='bg-white  shadow-md border-[3px]  border-b-0 border-green-300  '>
+      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        <Link to='/'>
+          <h1 className='font-bold text-3xl sm:text-xl flex flex-wrap bg-grey-200  rounded-xl p-1 hover:bg-black [&>span.thisone]:hover:text-white'>
+            <span className='text-green-700 '>Your</span>
+            <span className='thisone text-black '>Home</span>
+          </h1>
+        </Link>
+        <form
+          onSubmit={handleSubmit}
+          className='bg-slate-100 p-3 rounded-lg flex items-center'
+        >
+          <input
+            type='text'
+            placeholder='Search...'
+            className='bg-transparent focus:outline-none w-24 sm:w-64'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button>
+            <FaSearch className='text-slate-600' />
+          </button>
+        </form>
+        <ul className='flex gap-4'>
+          <Link to='/'>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>
+              Home
+            </li>
+          </Link>
+          <Link to='/about'>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>
+              About
+            </li>
+          </Link>
+          <Link to='/profile'>
+            {currentUser ? (
+              <img
+                className='rounded-full h-7 w-7 object-cover'
+                src={currentUser.avatar}
+                alt='profile'
+              />
+            ) : (
+              <li className=' text-slate-700 hover:underline'> Sign in</li>
+            )}
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+}
---
+++ client/src/components/OAuth.jsx
@@ -0,0 +1,13 @@
+
+
+export default function OAuth() {  
+  return (
+    <button
+    disabled
+      type='button'
+      className='bg-black text-white p-3 rounded-lg uppercase hover:opacity-95'
+    >
+      Continue with google <p className='text-sm text-green-200 font-bold'> <span className='text-white text-lg'>[ </span> Disabled for now <span className='text-white text-lg'>]</span></p>
+    </button>
+  );
+}
